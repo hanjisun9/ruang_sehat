6ebe08d99bafe9ffba017325d0fca36547cf7227
jujuu <azizahnf85@gmail.com>
2026-03-11 20:24:54 +0700

    second

--- lib/features/auth/presentation/widget/auth_form.dart
+++ lib/features/auth/presentation/widget/auth_form.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ruang_sehat/theme/app_colors.dart';
 
-class AuthForm extends StatefulWidget{
+class AuthForm extends StatefulWidget {
   final bool isLogin;
   final VoidCallback onSwitchToLogin;
 
@@ -10,7 +10,7 @@ class AuthForm extends StatefulWidget{
     super.key,
     required this.isLogin,
     required this.onSwitchToLogin,
-    });
+  });
 
   @override
   State<AuthForm> createState() => _AuthFormState();
@@ -28,32 +28,36 @@ class _AuthFormState extends State<AuthForm> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: 18),
-        Text(
-          'Name',
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-        ),
-        const SizedBox(height: 10),
-        TextFormField(
-          controller: nameController,
-          decoration: InputDecoration(
-            hintText: 'Enter Your Name',
-            hintStyle: TextStyle(color: AppColors.hintText),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: const BorderSide(color: AppColors.border, width: 2),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: const BorderSide(color: AppColors.primary, width: 2),
-            ),
-            contentPadding: const EdgeInsets.symmetric(
-              vertical: 15.0,
-              horizontal: 14.0
+        if (widget.isLogin == false) ...[
+          SizedBox(height: 18),
+          Text(
+            'Name',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            controller: nameController,
+            decoration: InputDecoration(
+              hintText: 'Enter Your Name',
+              hintStyle: TextStyle(color: AppColors.hintText),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(color: AppColors.border, width: 2),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: const BorderSide(
+                  color: AppColors.primary,
+                  width: 2,
+                ),
+              ),
+              contentPadding: const EdgeInsets.symmetric(
+                vertical: 15.0,
+                horizontal: 14.0,
+              ),
             ),
           ),
-        ),
-
+        ],
         SizedBox(height: 18),
         Text(
           'Username',
@@ -71,12 +75,12 @@ class _AuthFormState extends State<AuthForm> {
             ),
             focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(12),
-              borderSide: const BorderSide(color: AppColors.primary, width:2)
+              borderSide: const BorderSide(color: AppColors.primary, width: 2),
             ),
             contentPadding: const EdgeInsets.symmetric(
               vertical: 15.0,
-              horizontal: 14.0
-            )
+              horizontal: 14.0,
+            ),
           ),
         ),
 
@@ -98,11 +102,11 @@ class _AuthFormState extends State<AuthForm> {
             ),
             focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(12),
-              borderSide: const BorderSide(color: AppColors.primary, width: 2)
+              borderSide: const BorderSide(color: AppColors.primary, width: 2),
             ),
             contentPadding: const EdgeInsets.symmetric(
               vertical: 15.0,
-              horizontal: 14.0
+              horizontal: 14.0,
             ),
             suffixIcon: IconButton(
               icon: Icon(
@@ -114,7 +118,7 @@ class _AuthFormState extends State<AuthForm> {
                   _isObscure = !_isObscure;
                 });
               },
-            )
+            ),
           ),
         ),
 
@@ -126,88 +130,86 @@ class _AuthFormState extends State<AuthForm> {
             minimumSize: Size(double.infinity, 53),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadiusGeometry.circular(12),
-            )
+            ),
           ),
           child: Text(
             widget.isLogin ? 'Login' : 'Register',
             style: TextStyle(
               color: AppColors.secondary,
               fontSize: 16,
-              fontWeight: FontWeight.bold
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
 
-        SizedBox(height: 18),
-        Row(
-          children: [
-            Checkbox(
-              value: true,
-              onChanged: (value) {
-                setState(() {
-                  _rememberMe = value ?? false;
-                });
-              }, 
-              activeColor: AppColors.primary,
-              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              visualDensity: VisualDensity.compact,
-              side: const BorderSide(color: AppColors.hintText, width: 2),
-            ),
-            const Text(
-              'Remember me',
-              style: TextStyle(
-                color: AppColors.hintText,
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
+        if (widget.isLogin) ...[
+          SizedBox(height: 18),
+          Row(
+            children: [
+              Checkbox(
+                value: true,
+                onChanged: (value) {
+                  setState(() {
+                    _rememberMe = value ?? false;
+                  });
+                },
+                activeColor: AppColors.primary,
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                visualDensity: VisualDensity.compact,
+                side: const BorderSide(color: AppColors.hintText, width: 2),
               ),
-            ),
-            const Spacer(),
-            const Text(
-              'Forgot Password?',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                decoration: TextDecoration.underline
-              ),
-            )
-          ],
-        ),
-
-        SizedBox(height: 18),
-        Row(
-          children: [
-            Expanded(child: Divider(thickness: 1, color: AppColors.border)),
-            Padding(
-              padding: EdgeInsetsGeometry.symmetric(horizontal: 10.0),
-              child: Text(
-                'Or login with',
+              const Text(
+                'Remember me',
                 style: TextStyle(
                   color: AppColors.hintText,
+                  fontSize: 16,
                   fontWeight: FontWeight.w400,
                 ),
               ),
-            ),
-            Expanded(child: Divider(thickness: 1, color: AppColors.border)),
-          ],
-        ),
-
-        SizedBox(height: 18),
-        Center(
-          child: Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              color: AppColors.secondary.withOpacity(0.3),
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(color: AppColors.border, width: 2)
-            ),
-            child: Center(
-              child: SvgPicture.asset(
-                'assets/icons/google.svg',
+              const Spacer(),
+              const Text(
+                'Forgot Password?',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  decoration: TextDecoration.underline,
+                ),
               ),
+            ],
+          ),
+
+          SizedBox(height: 18),
+          Row(
+            children: [
+              Expanded(child: Divider(thickness: 1, color: AppColors.border)),
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 10.0),
+                child: Text(
+                  'Or login with',
+                  style: TextStyle(
+                    color: AppColors.hintText,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              Expanded(child: Divider(thickness: 1, color: AppColors.border)),
+            ],
+          ),
+
+          SizedBox(height: 18),
+          Center(
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: AppColors.secondary.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(color: AppColors.border, width: 2),
+              ),
+              child: Center(child: SvgPicture.asset('assets/icons/google.svg')),
             ),
           ),
-        )
+        ],
       ],
     );
   }
